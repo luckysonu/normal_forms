3490fb1d1050b886d295392ada7c98081cae1220
luckysonu <luckysonu466@gmail.com>
2022-12-21 13:40:21 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -24,4 +24,23 @@ def insert_webpage(request):
         W=Webpage.objects.get_or_create(topic_name=T,name=na,url=ur)[0]
         W.save()
         return HttpResponse('Webpage is inserted successfully')
-    return render(request,'insert_webpage.html',d)    
+    return render(request,'insert_webpage.html',d)  
+
+def select_topic(request):
+    topics=Topic.objects.all()
+    d={'topic':topics}
+
+    if request.method=='POST':
+        tn=request.POST.getlist('topic')
+        print(tn)
+        webpages=Webpage.objects.none()
+        for i in tn:
+            webpages=webpages|Webpage.objects.filter(topic_name=i) 
+        data={'webpages':webpages}
+        return render(request,'display_webpage.html',data)
+    return render(request,'select_topic.html',d)
+    
+def checkbox(request):
+    topics=Topic.objects.all()
+    d={'topics':topics}
+    return render(request,'checkbox.html',d)                 
